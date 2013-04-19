9e996ddcaf9e17d35e8f9600aad298c508d3fe61
Sean Escriva <sean.escriva@gmail.com>
2013-04-19 12:46:33 -0700

    setup deploy and config.rb changes

--- Gemfile
+++ Gemfile
@@ -1,5 +1,9 @@
-source :rubygems
+source 'https://rubygems.org'
 
 gem "middleman", "~>3.0.0.rc.3"
+gem "middleman-deploy"
 gem "sass-rails"
 gem "bootstrap-sass"
+gem "compass-susy-plugin"
+
+
--- config.rb
+++ config.rb
@@ -1,16 +1,12 @@
-### 
+###
 # Compass
 ###
 
 # Bootstrap
-# First: 
-#    gem install sass-rails
-#    gem install bootstrap-sass
 require 'bootstrap-sass'
 
 # Susy grids in Compass
-# First: gem install compass-susy-plugin
-# require 'susy'
+require 'susy'
 
 # Change Compass configuration
 compass_config do |config|
@@ -21,68 +17,30 @@ compass_config do |config|
 end
 
 ###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-# 
-# With no layout
-# page "/path/to/file.html", :layout => false
-# 
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-# 
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
-
-###
 # Helpers
 ###
 
 # Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+activate :automatic_image_sizes
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'js'
-
 set :images_dir, 'img'
 
 activate :directory_indexes
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
   activate :minify_css
-  
-  # Minify Javascript on build
   activate :minify_javascript
-  
   # Enable cache buster
   # activate :cache_buster
-  
-  # Use relative URLs
   activate :relative_assets
-  
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-  
   # Or use a different image path
-  set :http_path, "/img/"
+   set :http_path, "/img/"
+end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.branch = "master"
 end
