70166e4ce7f3eeecfad0ccdaacdbfb8c3b7d87c2
Sean Escriva <sean.escriva@gmail.com>
2013-04-19 18:42:27 -0700

    Automated commit at 2013-04-20 01:42:27 UTC by middleman-deploy 3.0.13

---
+++ js/plugins.js
@@ -0,0 +1,3 @@
+// usage: log('inside coolFunc', this, arguments);
+// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+window.log=function(){log.history=log.history||[],log.history.push(arguments);if(this.console){arguments.callee=arguments.callee.caller;var e=[].slice.call(arguments);typeof console.log=="object"?log.apply.call(console.log,console,e):console.log.apply(console,e)}},function(e){function t(){}for(var n="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,timeStamp,profile,profileEnd,time,timeEnd,trace,warn".split(","),r;r=n.pop();)e[r]=e[r]||t}(function(){try{return console.log(),window.console}catch(e){return window.console={}}}());
---
+++ test/tests.js
@@ -0,0 +1,4 @@
+// documentation on writing tests here: http://docs.jquery.com/QUnit
+// example tests: https://github.com/jquery/qunit/blob/master/test/same.js
+// below are some general tests but feel free to delete them.
+module("example tests"),test("HTML5 Boilerplate is sweet",function(){expect(1),equals("boilerplate".replace("boilerplate","sweet"),"sweet","Yes. HTML5 Boilerplate is, in fact, sweet")}),test("Environment is good",function(){expect(3),ok(!!window.log,"log function present");var e=log.history&&log.history.length||0;log("logging from the test suite."),equals(log.history.length-e,1,"log history keeps track"),ok(!!window.Modernizr,"Modernizr global is present")});
